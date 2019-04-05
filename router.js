eb693299276bdd54ac851343500243ec4043982d
Manuraj <manuraj.m@experionglobal.com>
2019-04-05 12:33:19 +0530

    Modified some files

--- router.js
+++ router.js
@@ -1,21 +1,20 @@
 
 let glob = require("glob");
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    global.__base = __dirname + '/';
+	global.__base = __dirname + '/';
 
-    // options is optional
-    let options = {};
+	// options is optional
+	let options = {};
 
-    glob("**/router.*.js", options , function (er, files) {
-        // files is an array of filenames.
-        // If the `nonull` option is set, and nothing
-        // was found, then files is ["**/*.js"]
-        // er is an error object or null.
-        
-        files.forEach(function(file){
-            app.use('/', require(__base+file));
-        });
-    });
+	glob("**/*Router.js", options, function (err, files) {
+		// files is an array of filenames.
+		// If the `nonull` option is set, and nothing
+		// was found, then files is ["**/*.js"]
+		// err is an error object or null.
+		files.forEach(function (file) {
+			app.use(`/${process.env.VERSION}/`, require(__base + file));
+		});
+	});
 }
